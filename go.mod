5e38c8c509ca6ea8a38855dcad7ef48adca3b25e
dairlair <dairlair@my.com>
2019-06-07 18:09:28 +0300

    Server skeleton created

--- go.mod
+++ go.mod
@@ -4,9 +4,14 @@ go 1.12
 
 require (
 	github.com/bxcodec/faker/v3 v3.1.0
+	github.com/cockroachdb/apd v1.1.0 // indirect
 	github.com/golang/protobuf v1.3.1
+	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
+	github.com/jackc/pgx v3.4.0+incompatible
+	github.com/lib/pq v1.1.1 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
+	github.com/shopspring/decimal v0.0.0-20180709203117-cd690d0c9e24 // indirect
 	github.com/sirupsen/logrus v1.2.0
-	github.com/spf13/cobra v0.0.4 // indirect
 	github.com/spf13/viper v1.4.0
 	github.com/stretchr/testify v1.2.2
 	golang.org/x/sys v0.0.0-20190422165155-953cdadca894 // indirect
--- pkg/cmd/server/server.go
+++ pkg/cmd/server/server.go
@@ -1,5 +1,13 @@
 package server
 
+import (
+	"errors"
+	"fmt"
+
+	"github.com/jackc/pgx"
+	log "github.com/sirupsen/logrus"
+)
+
 // PostgresConfig contains options for the Postgres connections pool
 type PostgresConfig struct {
 	DSN string
@@ -8,4 +16,52 @@ type PostgresConfig struct {
 // Config is configuration for the Server
 type Config struct {
 	Postgres PostgresConfig
+}
+
+// Instance stores the server state
+type Instance struct {
+	config   *Config
+	connPool *pgx.ConnPool
+}
+
+// NewInstance creates new server instance and copy config into that.
+func NewInstance(config *Config) *Instance {
+	s := &Instance{
+		config: config,
+	}
+	return s
+}
+
+func createPostgresConnection(config PostgresConfig) *pgx.ConnPool {
+	pgConf, err := pgx.ParseURI(config.DSN)
+	if err != nil {
+		msg := fmt.Sprintf("Can not parse Postgres DSN: %s", err)
+		panic(errors.New(msg))
+	}
+	log.Infof("PostgreSQL: host=%s, port=%d, username=%s, database=%s",
+		pgConf.Host,
+		pgConf.Port,
+		pgConf.User,
+		pgConf.Database,
+	)
+	connPool, err := pgx.NewConnPool(pgx.ConnPoolConfig{
+		ConnConfig: pgx.ConnConfig{
+			Host:     pgConf.Host,
+			Port:     pgConf.Port,
+			User:     pgConf.User,
+			Password: pgConf.Password,
+			Database: pgConf.Database,
+			RuntimeParams: map[string]string{
+				"application_name": "twitwatch",
+			},
+		},
+		MaxConnections: 1,
+	})
+
+	if err != nil {
+		msg := fmt.Sprintf("Can not connect to Postgres: %s", err)
+		panic(errors.New(msg))
+	}
+
+	return connPool
 }
