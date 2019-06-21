f6334ac3de0ced9c8b0da512bb339c32e89586b8
dairlair <dairlair@my.com>
2019-06-21 16:20:13 +0300

    Storage Interface mock updated

--- pkg/storage/mocks/Interface.go
+++ pkg/storage/mocks/Interface.go
@@ -52,3 +52,26 @@ func (_m *Interface) AddTwit(twit *v1.Twit) (int64, error) {
 
 	return r0, r1
 }
+
+// GetStreams provides a mock function with given fields:
+func (_m *Interface) GetStreams() ([]*v1.Stream, error) {
+	ret := _m.Called()
+
+	var r0 []*v1.Stream
+	if rf, ok := ret.Get(0).(func() []*v1.Stream); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]*v1.Stream)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func() error); ok {
+		r1 = rf()
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
