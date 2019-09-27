b3e4f0b2966f3a6210d118b086a826a8b60f15d9
Alexander Emelianov <Alexander.Emelianov@forextime.com>
2019-09-27 11:57:46 +0300

    The e2e test for signup fixed

--- tests/e2e/test/signup.test.ts
+++ tests/e2e/test/signup.test.ts
@@ -15,8 +15,9 @@ it('Should POST /signup return 2000 and id and email with valid credentials', as
         .post('/signup')
         .send({email: email, password: password});
 
-    expect(res.body).has.property("id").greaterThan(0);
     expect(res.body).has.property("email").eq(email);
+    expect(res.body).has.property("id").greaterThan(0);
+    expect(res.body).has.property("token").not.eq("");
 });
 
 it('Should POST /signup return 422 for missing email', async function () {   
@@ -25,8 +26,7 @@ it('Should POST /signup return 422 for missing email', async function () {
         .send({password: password})
         .expect(422);
 
-    expect(res.body).not.has.property("id");
-    expect(res.body).not.has.property("email");
+    expect(res.body).not.has.property("token");
     expect(res.body).has.property("message").eq("email in body is required");
 });
 
@@ -36,8 +36,7 @@ it('Should POST /signup return 422 for missing password', async function () {
         .send({email: email})
         .expect(422);
 
-    expect(res.body).not.has.property("id");
-    expect(res.body).not.has.property("email");
+    expect(res.body).not.has.property("token");
     expect(res.body).has.property("message").eq("password in body is required");
 });
 
@@ -47,7 +46,6 @@ it('Should POST /signup return 422 for email already taken', async function () {
         .send({email: email, password: password})
         .expect(422);
 
-    expect(res.body).not.has.property("id");
-    expect(res.body).not.has.property("email");
+    expect(res.body).not.has.property("token");
     expect(res.body).has.property("message").eq("Email already taken");
 });
